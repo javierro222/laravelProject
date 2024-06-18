ba4782e4bbd30f0805a7dafd668242213678f82f
javierro22 <fjavierrodcan@gmail.com>
2024-06-18 18:25:38 +0200

    creacion de crud solucion de errores varios

--- laravel-crud-api/routes/web.php
+++ laravel-crud-api/routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 // Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/usuarios', [App\Http\Controllers\UserController::class, 'index'])->name('usuarios');
+Route::get('users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+Route::get('users/store', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
